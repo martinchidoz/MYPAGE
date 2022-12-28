590ec438ffb2d6fa798072f70ae25065da5b76f5
martinchidoz <chidozieonwumere@gmail.com>
2022-12-28 11:46:14 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+
+import React, {lazy, Suspense} from "react";
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+import { Spinner,  } from "reactstrap";
+const Home = lazy(() => import("./Pages/Home"))
+const Contact = lazy(() => import("./Pages/Contact"))
+const About =lazy(() => import("./Pages/About"))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+  return ( 
+    <Suspense fallback={<Spinner
+       type ={"grow"} className="text-primary position-absolute top-50 start-50 translate-middle"/>}>
+ <BrowserRouter>
+ <Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/about" element={<About/>}/>
+  <Route path="/contact" element ={<Contact/>}/>
+  </Routes></BrowserRouter></Suspense>
   );
 }
 
